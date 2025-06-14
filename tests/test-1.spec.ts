eb3f504511fb567d6d833e00cd677609ede000c1
TonySelvan <dktselvan@gmail.com>
2025-06-14 12:05:40 +0530

    Delete

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/?gws_rd=ssl');
-  await page.getByRole('combobox', { name: 'Search' }).click();
-  await page.getByRole('combobox', { name: 'Search' }).fill('playw');
-  await page.getByText('playwright documentation').click();
-});
